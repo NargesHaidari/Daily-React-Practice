d8e051c3f8103600487e8f857eeecc07c9e00107
Narges Haidari <nargeshaidari2026@hotmail.com>
2026-05-31 06:54:34 +0500

    Learn React children props

--- React-practice/src/App.jsx
+++ React-practice/src/App.jsx
@@ -1,11 +1,25 @@
 import './App.css'
-import SchoolOffice from './components/SchoolOffice'
-
+import Card from "./components/Card";
+import ReactDeveloper from './assets/React-developer.jpg'
 
 function App() {
   return (
-    <SchoolOffice/>
-  )
+    <>
+      <Card>
+        <h2>درباره من</h2>
+        <p>Frontend Developer</p>
+      </Card>
+
+      <Card>
+        <img className="card-image" src={ReactDeveloper} alt="profile"/>
+        <p>React Developer</p>
+      </Card>
+
+      <Card>
+        <button>Click Me</button>
+      </Card>
+    </>
+  );
 }
 
-export default App
+export default App;
